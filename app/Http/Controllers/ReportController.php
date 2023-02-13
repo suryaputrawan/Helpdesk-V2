bd1ac0dc49db99fe11e0dbf8b4bf821d76efc452
Ida Bagus Surya Putrawan <suryaputrawan@gmail.com>
2023-02-13 11:26:23 +0800

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,19 +30,19 @@ class ReportController extends Controller
                         },
                         'userTechnician' => function ($query) {
                             $query->select('id', 'name');
+                        },
+                        'office' => function ($query) {
+                            $query->select('id', 'name');
                         }
                     ])
                     //Filter data berdasarkan 2 tanggal
                     ->when(request('start_date') != null && request('end_date') != null, function ($query) {
                         return  $query->whereBetween(DB::raw('DATE(date)'), [request('start_date'), request('end_date')]);
                     })
-                    //Filter data berelasi
-                    ->when(request('office_id') != null && request('office_id') != 'NaN', function ($query) {
-                        return $query->whereHas('userRequester', function ($q) {
-                            $q->where('office_id', request('office_id'));
-                        });
-                    })
                     //Filter data tanpa relasi
+                    ->when(request('office_id') != null && request('office_id') != 'NaN', function ($query) {;
+                        return $query->where('office_id', '=', request('office_id'));
+                    })
                     ->when(request('category_id') != null && request('category_id') != 'NaN', function ($query) {
                         return $query->where('category_id', '=', request('category_id'));
                     })
@@ -56,6 +56,7 @@ class ReportController extends Controller
                         'title',
                         'category_id',
                         'location_id',
+                        'office_id',
                         'status_id',
                         'requester_id',
                         'technician_id',
@@ -89,7 +90,7 @@ class ReportController extends Controller
                         return $data->userRequester ? $data->userRequester->name : '';
                     })
                     ->addColumn('office', function ($data) {
-                        return $data->userRequester->office->name;
+                        return $data->office->name;
                     })
                     ->addColumn('interval', function ($data) {
                         //Mencari interval waktu
@@ -120,13 +121,13 @@ class ReportController extends Controller
                     ->make(true);
             }
             return view('report.technician.index', [
-                'office'        => Office::get(),
-                'status'        => Status::get(),
+                'office'        => Office::get(['id', 'name']),
+                'status'        => Status::get(['id', 'name']),
                 'category'      => Category::where('department_id', '=', auth()->user()->department_id)->get()
             ]);
-        } else {
+        } elseif (auth()->user()->role == "Management") {
             if (request()->type == 'datatable') {
-                $data = Ticket::latest()
+                $data = Ticket::where('office_id', auth()->user()->office_id)->latest()
                     ->with([
                         'status' => function ($query) {
                             $query->select('id', 'name');
@@ -136,19 +137,19 @@ class ReportController extends Controller
                         },
                         'userTechnician' => function ($query) {
                             $query->select('id', 'name');
+                        },
+                        'office' => function ($query) {
+                            $query->select('id', 'name');
                         }
                     ])
                     //Filter data berdasarkan 2 tanggal
                     ->when(request('start_date') != null && request('end_date') != null, function ($query) {
                         return  $query->whereBetween(DB::raw('DATE(date)'), [request('start_date'), request('end_date')]);
                     })
-                    //Filter data berelasi
-                    ->when(request('office_id') != null && request('office_id') != 'NaN', function ($query) {
-                        return $query->whereHas('userRequester', function ($q) {
-                            $q->where('office_id', request('office_id'));
-                        });
-                    })
                     //Filter data tanpa relasi
+                    ->when(request('office_id') != null && request('office_id') != 'NaN', function ($query) {
+                        return $query->where('office_id', '=', request('office_id'));
+                    })
                     ->when(request('category_id') != null && request('category_id') != 'NaN', function ($query) {
                         return $query->where('category_id', '=', request('category_id'));
                     })
@@ -162,6 +163,7 @@ class ReportController extends Controller
                         'title',
                         'category_id',
                         'location_id',
+                        'office_id',
                         'status_id',
                         'requester_id',
                         'technician_id',
@@ -198,7 +200,7 @@ class ReportController extends Controller
                         return $data->userTechnician ? $data->userTechnician->name : '';
                     })
                     ->addColumn('office', function ($data) {
-                        return $data->userRequester->office->name;
+                        return $data->office->name;
                     })
                     ->addColumn('interval', function ($data) {
                         //Mencari interval waktu
@@ -238,9 +240,128 @@ class ReportController extends Controller
                     ->make(true);
             }
             return view('report.index', [
-                'office'        => Office::get(),
+                'office'        => Office::where('id', auth()->user()->office_id)->get(['id', 'name']),
                 'technician'    => User::where('role', 'Technician')->get(),
-                'category'      => Category::get()
+                'category'      => Category::get(['id', 'name'])
+            ]);
+        } else {
+            if (request()->type == 'datatable') {
+                $data = Ticket::latest()
+                    ->with([
+                        'status' => function ($query) {
+                            $query->select('id', 'name');
+                        },
+                        'userRequester' => function ($query) {
+                            $query->select('id', 'name', 'office_id');
+                        },
+                        'userTechnician' => function ($query) {
+                            $query->select('id', 'name');
+                        },
+                        'office' => function ($query) {
+                            $query->select('id', 'name');
+                        }
+                    ])
+                    //Filter data berdasarkan 2 tanggal
+                    ->when(request('start_date') != null && request('end_date') != null, function ($query) {
+                        return  $query->whereBetween(DB::raw('DATE(date)'), [request('start_date'), request('end_date')]);
+                    })
+                    //Filter data tanpa relasi
+                    ->when(request('office_id') != null && request('office_id') != 'NaN', function ($query) {
+                        return $query->where('office_id', '=', request('office_id'));
+                    })
+                    ->when(request('category_id') != null && request('category_id') != 'NaN', function ($query) {
+                        return $query->where('category_id', '=', request('category_id'));
+                    })
+                    ->when(request('technician_id') != null && request('technician_id') != 'NaN', function ($query) {
+                        return $query->where('technician_id', '=', request('technician_id'));
+                    })
+                    ->select(
+                        'id',
+                        'nomor',
+                        'date',
+                        'title',
+                        'category_id',
+                        'location_id',
+                        'office_id',
+                        'status_id',
+                        'requester_id',
+                        'technician_id',
+                        'assign_date',
+                        'solved_date'
+                    )->get();
+
+                return datatables()->of($data)
+                    ->editColumn('date', function ($data) {
+                        $date = new DateTime($data->date);
+                        return $data->date ? $date->format('d M Y') : '';
+                    })
+                    ->editColumn('assign_date', function ($data) {
+                        $date = new DateTime($data->assign_date);
+                        return $data->assign_date ? $date->format('d M Y H:i:s') : '';
+                    })
+                    ->editColumn('solved_date', function ($data) {
+                        $date = new DateTime($data->solved_date);
+                        return $data->solved_date ? $date->format('d M Y H:i:s') : '';
+                    })
+                    ->addColumn('category', function ($data) {
+                        return $data->category ? $data->category->name : '';
+                    })
+                    ->addColumn('location', function ($data) {
+                        return $data->location ? $data->location->name : '';
+                    })
+                    ->addColumn('status', function ($data) {
+                        return $data->status ? $data->status->name : '';
+                    })
+                    ->addColumn('requester', function ($data) {
+                        return $data->userRequester ? $data->userRequester->name : '';
+                    })
+                    ->addColumn('technician', function ($data) {
+                        return $data->userTechnician ? $data->userTechnician->name : '';
+                    })
+                    ->addColumn('office', function ($data) {
+                        return $data->office->name;
+                    })
+                    ->addColumn('interval', function ($data) {
+                        //Mencari interval waktu
+                        $assignDate = new DateTime($data->assign_date);
+                        $solvedDate = new DateTime($data->solved_date);
+                        $interval = $assignDate->diff($solvedDate);
+                        $year = $interval->y;
+                        $month = $interval->m;
+                        $day = $interval->d;
+                        $hour = $interval->format('%H');
+                        $minute = $interval->format('%I');
+                        $second = $interval->format('%S');
+
+                        if ($year != 0) {
+                            $ticketTime = $year . 'y ' . $month . 'm ' .   $day . 'd ' . $hour . ':' . $minute . ':' . $second;
+                        } elseif ($month != 0) {
+                            $ticketTime = $month . 'm ' .   $day . 'd ' . $hour . ':' . $minute . ':' . $second;
+                        } else if ($day != 0) {
+                            $ticketTime = $day . 'd ' . $hour . ':' . $minute . ':' . $second;
+                        } else {
+                            $ticketTime = $hour . ':' . $minute . ':' . $second;
+                        }
+                        //End interval waktu
+
+                        return $data->solved_date ? '<span style="color:red">' . $ticketTime . '</span>' : '';
+                    })
+                    ->addColumn('resolution', function ($data) {
+                        if ($data->ticketProgress != Null) {
+                            foreach ($data->ticketProgress as $progress) {
+                                return $progress->description;
+                            }
+                        } else {
+                            return '';
+                        }
+                    })
+                    ->rawColumns(['interval', 'resolution'])
+                    ->make(true);
+            }
+            return view('report.index', [
+                'office'        => Office::get(['id', 'name']),
+                'technician'    => User::where('role', 'Technician')->get(),
+                'category'      => Category::get(['id', 'name'])
             ]);
         }
     }
@@ -263,6 +384,9 @@ class ReportController extends Controller
                     'ticketProgress' => function ($query) {
                         $query->select('id', 'ticket_id', 'description');
                     },
+                    'office' => function ($query) {
+                        $query->select('id', 'name');
+                    }
                 ])
                 //Filter data berdasarkan 2 tanggal
                 ->when(request('start_date') != null && request('end_date') != null, function ($query) {
@@ -270,9 +394,7 @@ class ReportController extends Controller
                 })
                 //Filter data berelasi
                 ->when(request('office') != null && request('office') != 'NaN', function ($query) {
-                    return $query->whereHas('userRequester', function ($q) {
-                        $q->where('office_id', request('office'));
-                    });
+                    return $query->where('office_id', '=', request('office'));
                 })
                 //Filter data tanpa relasi
                 ->when(request('category') != null && request('category') != 'NaN', function ($query) {
@@ -288,6 +410,55 @@ class ReportController extends Controller
                     'title',
                     'category_id',
                     'location_id',
+                    'office_id',
+                    'status_id',
+                    'requester_id',
+                    'technician_id',
+                    'assign_date',
+                    'solved_date'
+                )->get();
+        } elseif (auth()->user()->role == "Management") {
+            $tickets = Ticket::where('office_id', auth()->user()->office_id)->latest()
+                ->with([
+                    'status' => function ($query) {
+                        $query->select('id', 'name');
+                    },
+                    'userRequester' => function ($query) {
+                        $query->select('id', 'name', 'office_id');
+                    },
+                    'userTechnician' => function ($query) {
+                        $query->select('id', 'name');
+                    },
+                    'ticketProgress' => function ($query) {
+                        $query->select('id', 'ticket_id', 'description');
+                    },
+                    'office' => function ($query) {
+                        $query->select('id', 'name');
+                    }
+                ])
+                //Filter data berdasarkan 2 tanggal
+                ->when(request('start_date') != null && request('end_date') != null, function ($query) {
+                    return  $query->whereBetween(DB::raw('DATE(date)'), [request('start_date'), request('end_date')]);
+                })
+                //Filter data berelasi
+                ->when(request('office') != null && request('office') != 'NaN', function ($query) {
+                    return $query->where('office_id', '=', request('office'));
+                })
+                //Filter data tanpa relasi
+                ->when(request('category') != null && request('category') != 'NaN', function ($query) {
+                    return $query->where('category_id', '=', request('category'));
+                })
+                ->when(request('technician_id') != null && request('technician_id') != 'NaN', function ($query) {
+                    return $query->where('technician_id', '=', request('technician'));
+                })
+                ->select(
+                    'id',
+                    'nomor',
+                    'date',
+                    'title',
+                    'category_id',
+                    'location_id',
+                    'office_id',
                     'status_id',
                     'requester_id',
                     'technician_id',
@@ -309,6 +480,9 @@ class ReportController extends Controller
                     'ticketProgress' => function ($query) {
                         $query->select('id', 'ticket_id', 'description');
                     },
+                    'office' => function ($query) {
+                        $query->select('id', 'name');
+                    }
                 ])
                 //Filter data berdasarkan 2 tanggal
                 ->when(request('start_date') != null && request('end_date') != null, function ($query) {
@@ -316,9 +490,7 @@ class ReportController extends Controller
                 })
                 //Filter data berelasi
                 ->when(request('office') != null && request('office') != 'NaN', function ($query) {
-                    return $query->whereHas('userRequester', function ($q) {
-                        $q->where('office_id', request('office'));
-                    });
+                    return $query->where('office_id', '=', request('office'));
                 })
                 //Filter data tanpa relasi
                 ->when(request('category') != null && request('category') != 'NaN', function ($query) {
@@ -334,6 +506,7 @@ class ReportController extends Controller
                     'title',
                     'category_id',
                     'location_id',
+                    'office_id',
                     'status_id',
                     'requester_id',
                     'technician_id',
@@ -341,7 +514,6 @@ class ReportController extends Controller
                     'solved_date'
                 )->get();
         }
-
         $pdf = PDF::loadview('report/export/ticketPdf', compact('tickets'))->setPaper('Legal', 'landscape');
         return $pdf->stream();
     }
